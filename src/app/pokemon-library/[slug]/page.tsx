a7bc72d0bf16f2e986320d414d36b4f3894b78b7
JaxV9 <layanj.pro@gmail.com>
2024-05-26 11:45:46 +0200

    feat: stats added

--- src/app/pokemon-library/[slug]/page.tsx
+++ src/app/pokemon-library/[slug]/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 import { PokemonSpecies } from '@/components/pokemonSpecies/pokemonSpecies';
+import { Stats } from '@/components/stats/stats';
+import { StatsType } from '@/model/stats';
 import { useParams } from 'next/navigation'
 import { useEffect, useState } from 'react';
 
@@ -8,6 +10,7 @@ export default function PokemonSheet() {
 
     const { slug } = useParams() as { slug: string }
     const [pokemon, setPokemon] = useState<PokemonDatas | null>(null)
+    const [stats, setStats] = useState<StatsType[]>([])
     const [currentLang, setCurrentLang] = useState<string>("en")
 
     const [currentNav, setCurrentNav] = useState<string>("about")
@@ -17,6 +20,7 @@ export default function PokemonSheet() {
         try {
             let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${slug}`, { cache: 'force-cache' })
             let data = await response.json()
+            setStats(data.stats)
             setPokemon(data);
         } catch {
             console.log("error")
@@ -35,21 +39,38 @@ export default function PokemonSheet() {
                         <div className='pokemonNameAndPictureContainer'>
                             <h1>{pokemon.name}</h1>
                             <img className="pokemonPicture" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} alt={pokemon.name} />
+                            <div>
+                                <div className='pokemonTypesContainer'>
+                                    {pokemon.types.length > 0 ?
+                                        pokemon.types.map((type, index) => (
+                                            <div className='pokemonTypeTag' key={index}>
+                                                <p>{type.type.name}</p>
+                                            </div>
+                                        ))
+                                        : null}
+                                </div>
+                            </div>
                         </div>
                         <div className='pokemonDatasContainer'>
                             <div className='navPokemonContainer'>
-                                <span className={currentNav === "about" ? "navPokemon focusNav": "navPokemon"}
-                                onClick={() => setCurrentNav("about")}>About</span>
-                                <span className={currentNav === "evolution" ? "navPokemon focusNav": "navPokemon"}
-                                onClick={() => setCurrentNav("evolution")}>Evolutions</span>
-                                <span className={currentNav === "stats" ? "navPokemon focusNav": "navPokemon"}
-                                onClick={() => setCurrentNav("stats")}>Stats</span>
-                                <span className={currentNav === "moves" ? "navPokemon focusNav": "navPokemon"}
-                                onClick={() => setCurrentNav("moves")}>Moves</span>
+                                <span className={currentNav === "about" ? "navPokemon focusNav" : "navPokemon"}
+                                    onClick={() => setCurrentNav("about")}>About</span>
+                                <span className={currentNav === "evolution" ? "navPokemon focusNav" : "navPokemon"}
+                                    onClick={() => setCurrentNav("evolution")}>Evolutions</span>
+                                <span className={currentNav === "stats" ? "navPokemon focusNav" : "navPokemon"}
+                                    onClick={() => setCurrentNav("stats")}>Stats</span>
+                                <span className={currentNav === "moves" ? "navPokemon focusNav" : "navPokemon"}
+                                    onClick={() => setCurrentNav("moves")}>Moves</span>
                             </div>
                             {
                                 currentNav === "about" ?
                                     <PokemonSpecies currentLangProps={currentLang} pokemonProps={pokemon} />
+                                    :
+                                    null
+                            }
+                                                        {
+                                currentNav === "stats" ?
+                                    <Stats statsProps={stats}/>
                                     :
                                     null
                             }
